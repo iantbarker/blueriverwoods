661198f0c9e575af00063ee99708429e91b4b293
Ian Barker <ianbarker@ihopkc.org>
2016-08-02 23:31:24 -0500

    Remove footer content

--- wp-content/themes/blue-river-woods/footer.php
+++ wp-content/themes/blue-river-woods/footer.php
@@ -13,13 +13,13 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
+<!-- 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'blue-river-woods' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'blue-river-woods' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'blue-river-woods' ), 'blue-river-woods', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<!--</footer> #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
